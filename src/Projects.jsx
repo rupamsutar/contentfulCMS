5017098f2f32f74a1e8697abc66c44d9583d466c
Rupam Sutar <rupamsutar281998@gmail.com>
2023-09-05 16:54:30 +0530

    useFetchProjects custom hook setup complete.

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
-import './fetchProjects';
+import {useFetchProjects} from './fetchProjects';
 
 const Projects = () => {
+  const {loading, projects} = useFetchProjects();
+  console.log(loading, projects)
   return (
     <div>Projects</div>
   )
--- src/fetchProjects.jsx
+++ src/fetchProjects.jsx
@@ -1,4 +1,5 @@
 import { createClient } from "contentful";
+import { useEffect, useState } from "react";
 
 const client = createClient({
     space: 'iqyg99c3w0mq',
@@ -7,6 +8,32 @@ const client = createClient({
     contentType: 'cms projects'
 });
 
-client.getEntries({content_type: 'cmsProjects'}).then((res) => {
-    console.log(res);
-})
+export const useFetchProjects = () => {
+    const [loading, setLoading] = useState(true);
+    const [projects, setProjects] = useState([]);
+
+    const getData = async () => {
+
+        try {
+            const response = await client.getEntries({ content_type: 'cmsProjects' });
+            const projects = response.items.map((item) => {
+                const { title, url, image } = item.fields;
+                const id = item.sys.id;
+                const img = image?.fields?.file?.url;
+
+                return { title, url, id, img }
+            })
+            setProjects(projects);
+            setLoading(false);
+        } catch (error) {
+            console.log(error);
+            setLoading(false);
+        }
+    }
+
+    useEffect(() => {
+        getData();
+    }, []);
+
+    return {loading, projects}
+}
